9d66aa739877e4a8c9a7df04fa0e353534d4e5c8
keySEE <museandmuse@gmail.com>
2015-04-22 22:35:43 +0400

    attempt 3
    added some comments for functions

--- cachematrix.R
+++ cachematrix.R
@@ -5,34 +5,34 @@
 ## Great explanation: https://class.coursera.org/rprog-013/forum/thread?thread_id=125#post-700
 
 
-makeCacheMatrix <- function(x = matrix()) {
-		m <- NULL
+makeCacheMatrix <- function(x = matrix()) {					##
+		m <- NULL											 # create 'm' variable
 		set <- function(y)
 					 {
                 				x <<- y
                 				m <<- NULL
       	 			  }
-        get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
+        get <- function() x									# Returns original matrix
+        setinverse <- function(solve) m <<- solve			# Modify existing matrix
+        getinverse <- function() m							# Returns matrix inverse
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 
 }
+ 
 
 
-
-
-cacheSolve <- function(x, ...) {
+# Returns cached matrix inverse using previously computed matrix inverse
+cacheSolve <- function(x, ...) {							 # This function a computes, caches, and returns matrix inverse
         ## Return a matrix that is the inverse of 'x'
- 	  m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+ 	  m <- x$getinverse()									 # Returns matrix inverse of matrix 'x' and assign to variable 'm'
+        if(!is.null(m)) {									 # Check for existing data in variabe 'm' ('m' is not NULL ). If this expression TRUE^
+                message("getting cached data")				 # We output information message "getting cached data"
+                return(m)								     # and return 'm' value
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        data <- x$get()									     # Get matrix
+        m <- solve(data, ...)								 # Computes matrix inverse
+        x$setinverse(m)										 # Modify existing matrix
+        m						
 }
